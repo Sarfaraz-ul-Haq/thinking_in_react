ab2efd904bbbc383541e7cea281a39516e0fd2f2
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-05-25 15:59:45 +0500

    make static components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,76 @@
-function ThinkingInReact() {
-  return <div className="p-10">Learning to Think in React</div>;
+"use client";
+import { useState } from "react";
+import PRODUCTS from "@/data/products";
+
+function ProductCategoryRow({ category }: any) {
+  return (
+    <tr>
+      <th colSpan={2}>{category}</th>
+    </tr>
+  );
 }
 
-export default ThinkingInReact;
+function ProductRow({ product }: any) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span className="text-red-800">{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products }: any) {
+  const rows: any = [];
+  let lastCategory: any = null;
+
+  products.forEach((product: any) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="string" placeholder="Search product" />
+      <label>
+        <input type="checkbox" /> Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+export default function FilterableProductTable() {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable products={PRODUCTS} />
+    </>
+  );
+}
